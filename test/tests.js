bff5e9442db67a9b81fd5657d0b54f3b5f9b3909
Martin Kleinschrodt <martin@maklesoft.com>
2014-01-22 18:41:28 +0100

    Add tests for secustore backup import

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,6 @@
 QUnit.config.autostart = false;
 
-require(["padlock/crypto", "padlock/util", "padlock/model"], function(crypto, util, model) {
+require(["padlock/crypto", "padlock/util", "padlock/model", "padlock/import"], function(crypto, util, model, imp) {
 
     module("padlock/crypto");
 
@@ -184,6 +184,36 @@ require(["padlock/crypto", "padlock/util", "padlock/model"], function(crypto, ut
 
         coll.save();
         ok(coll.exists(), "After the collection has been saved, it should exist ist the store.");
+    });
+
+    module("padlock/import");
+
+    asyncTest("import secustore set", function() {
+        var sample = "secustore-backup.txt",
+            password = "password",
+            expected = [
+                {name: "My Record", fields: [
+                    {name: "My field", value: "some value"}
+                ]},
+                {name: "Website", fields: [
+                    {name: "url", value: "http://test.org"},
+                    {name: "login name", value: "username"},
+                    {name: "password", value: "password"}
+                ]}
+            ];
+
+        expect(1);
+
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4) {
+                var data = imp.importSecuStoreBackup(xmlhttp.responseText, password);
+                deepEqual(data, expected, "Imported records did not match the exptected data");
+                start();
+            }
+        };
+        xmlhttp.open("GET", sample, true);
+        xmlhttp.send();
     });
 
     QUnit.start();
