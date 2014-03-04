70353a25807d14f55d68f6727791dbaef2532edd
Martin Kleinschrodt <martin@maklesoft.com>
2014-03-04 21:07:00 +0100

    Fix typo in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,5 +33,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('default', ['watch'], 'connect');
+    grunt.registerTask('default', ['watch']);
 };
