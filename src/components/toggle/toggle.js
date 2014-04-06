656986c402c7aefe9a7d3a8c5c1f83f98ecd34fc
Martin Kleinschrodt <martin@maklesoft.com>
2014-04-06 19:24:11 +0200

    Minor bugfix

--- src/components/toggle/toggle.js
+++ src/components/toggle/toggle.js
@@ -2,6 +2,7 @@ Polymer("padlock-toggle", {
     value: false,
     toggle: function() {
         this.value = !this.value;
+        this.valueChanged();
     },
     valueChanged: function() {
         if (this.value) {
