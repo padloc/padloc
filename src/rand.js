e2d4c25a62a326d13c70496bec57d1af5b6aedd5
Martin Kleinschrodt <martin@maklesoft.com>
2014-05-15 22:00:45 +0200

    Add module for randomization

---
+++ src/rand.js
@@ -0,0 +1,36 @@
+define(function() {
+    var chars = {
+        numbers: "0123456789",
+        lower: "abcdefghijklmnopqrstuvwxyz",
+        upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        other: "/+()%\"=&-!:'*#?;,_.@`~$^[{]}\\|<>"
+    };
+
+    var charSets = {
+        full: chars.numbers + chars.upper + chars.lower + chars.other,
+        alphanum: chars.numbers + chars.upper + chars.lower,
+        alpha: chars.lower + chars.upper,
+        num: chars.numbers,
+        hexa: chars.numbers + "abcdef"
+    };
+
+    //* Returns a random item from an array of a random char from a string
+    function randomItem(choices) {
+        return choices[Math.floor(Math.random()*choices.length)];
+    }
+
+    //* Creates a random string with a given _length_ comprised of given set or characters
+    function randomString(length, charSet) {
+        length = length || 32;
+        charSet = charSet || charSets.full;
+        var rndm = "";
+        while (rndm.length < length) {
+            rndm += randomItem(charSet);
+        }
+        return rndm;
+    }
+
+    return {
+        randomString: randomString
+    };
+});
