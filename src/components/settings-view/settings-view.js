0e6dfd0a84116c25e9784042fe8895bb32e07524
Martin Kleinschrodt <martin@maklesoft.com>
2015-07-30 05:43:51 +0200

    Use underscore convention for private methods in settings view

--- src/components/settings-view/settings-view.js
+++ src/components/settings-view/settings-view.js
@@ -21,14 +21,14 @@
             this.fire("back");
         },
         //* Opens the change password dialog and resets the corresponding input elements
-        changePassword: function() {
+        _changePassword: function() {
             this.$.changePasswordErrorDialog.open = false;
             this.$.currPwdInput.value = "";
             this.$.newPwdInput.value = "";
             this.$.confirmNewPwdInput.value = "";
             this.$.changePasswordDialog.open = true;
         },
-        confirmChangePassword: function() {
+        _confirmChangePassword: function() {
             this.$.changePasswordDialog.open = false;
             // TODO: Add a better check for the current password
             if (this.$.currPwdInput.value != this.collection.defaultPassword) {
@@ -43,37 +43,37 @@
                 this.$.changePasswordSuccessDialog.open = true;
             }
         },
-        closeChangePasswordErrorDialog: function() {
+        _closeChangePasswordErrorDialog: function() {
             this.$.changePasswordErrorDialog.open = false;
         },
-        closeChangePasswordSuccessDialog: function() {
+        _closeChangePasswordSuccessDialog: function() {
             this.$.changePasswordSuccessDialog.open = false;
         },
         //* Opens the dialog for connecting to the Padlock Cloud
-        cloudConnect: function() {
+        _cloudConnect: function() {
             this.$.emailInput.value = this.settings.sync_email || "";
             this.$.deviceNameInput.value = this.settings.sync_device || "";
             this.$.connectDialog.open = true;
         },
-        confirmConnect: function() {
+        _confirmConnect: function() {
             this.$.connectDialog.open = false;
             this.set("settings.sync_email", this.$.emailInput.value);
             this.set("settings.sync_device", this.$.deviceNameInput.value);
             this.requestApiKey();
         },
-        cloudDisconnect: function() {
+        _cloudDisconnect: function() {
             this.$.disconnectDialog.open = true;
         },
-        confirmDisconnect: function() {
+        _confirmDisconnect: function() {
             this.$.disconnectDialog.open = false;
             this.set("settings.sync_connected", false);
             this.set("settings.sync_key", "");
         },
-        cancelDisconnect: function() {
+        _cancelDisconnect: function() {
             this.$.disconnectDialog.close();
         },
         //* Requests an api key from the cloud api with the entered email and device name
-        requestApiKey: function() {
+        _requestApiKey: function() {
             var req = new XMLHttpRequest(),
                 url = this.settings.sync_host + "auth/",
                 email = this.$.emailInput.value,
@@ -107,32 +107,28 @@
             req.send("email=" + email + "&device_name=" + deviceName);
         },
         //* Shows an alert dialog with a given _message_
-        alert: function(message) {
+        _alert: function(message) {
             this.$.alertText.innerHTML = message;
             this.$.alertDialog.open = true;
         },
-        dismissAlert: function() {
+        _dismissAlert: function() {
             this.$.alertDialog.open = false;
         },
         //* Tap handler for the auto sync row. Toggles the auto sync toggle element
-        toggleAutoSync: function(event) {
+        _toggleAutoSync: function(event) {
             // Make sure the event is not coming from the toggle element itself as this
             // would result in the element being toggled twice
             if (event.target != this.$.autoSyncToggle) {
                 this.$.autoSyncToggle.toggle();
             }
         },
-        //* Saves the current settings
-        save: function() {
-            this.settings && this.settings.loaded && this.settings.save();
-        },
-        import: function() {
+        _import: function() {
             this.fire("import");
         },
-        openWebsite: function() {
+        _openWebsite: function() {
             window.open("http://padlock.io", "_system");
         },
-        sendMail: function() {
+        _sendMail: function() {
             var url = "mailto:support@padlock.io";
 
             // window.location = "mailto:..." won't work in packaged chrome apps so we have to use window.open
@@ -142,14 +138,14 @@
                 window.location = url;
             }
         },
-        openGithub: function() {
+        _openGithub: function() {
             window.open("http://github.com/maklesoft", "_system");
         },
-        resetData: function() {
+        _resetData: function() {
             this.$.resetConfirmPwd.value = "";
             this.$.resetDataDialog.open = true;
         },
-        confirmResetData: function() {
+        _confirmResetData: function() {
             this.$.resetDataDialog.open = false;
 
             if (this.$.resetConfirmPwd.value == this.collection.defaultPassword) {
@@ -160,13 +156,13 @@
                 this.alert("The password you entered was incorrect.");
             }
         },
-        cancelResetData: function() {
+        _cancelResetData: function() {
             this.$.resetDataDialog.open = false;
         },
-        resetRemoteData: function() {
+        _resetRemoteData: function() {
             this.$.resetRemoteDataDialog.open = true;
         },
-        confirmResetRemoteData: function() {
+        _confirmResetRemoteData: function() {
             this.$.resetRemoteDataDialog.open = false;
 
             var req = new XMLHttpRequest(),
@@ -192,7 +188,7 @@
             req.open("DELETE", url, true);
             req.send();
         },
-        cancelResetRemoteData: function() {
+        _cancelResetRemoteData: function() {
             this.$.resetRemoteDataDialog.open = false;
         }
     });
