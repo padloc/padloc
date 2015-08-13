5823e612188ff9704b98692154fd0dea2538db1b
Martin Kleinschrodt <martin@maklesoft.com>
2015-08-13 14:14:36 +0200

    Restyle dialog component; make toaster transitions consistent across app

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -1,6 +1,6 @@
-/* global Polymer, padlock */
+/* global Polymer */
 
-(function(Polymer, platform) {
+(function(Polymer) {
     "use strict";
 
     Polymer({
@@ -13,40 +13,20 @@
             }
         },
         listeners: {
-            tap: "close",
-            transitionend: "_transitionEnd"
+            tap: "_close"
         },
         //* Changed handler for the _open_ property. Shows/hides the dialog
         _openChanged: function() {
-            var items = Polymer.dom(this).children,
-                // transition delay between individual items
-                dt = 0.2/items.length,
-                prefix = platform.getVendorPrefix().css,
-                l = items.length,
-                delay;
-
-            // Firefox apparently doesn't want a prefix when setting the style property directly via the dom
-            prefix = prefix == "-moz-" ? "" : prefix;
-
-            // Apply incremental transition delays to the individual elements
-            // to create a nice animation
-            for (var i=0; i<l; i++) {
-                delay = this.open ? i * dt : (items.length - i - 1) * dt;
-                items[i].style[prefix + "transition"] = prefix + "transform 0.3s ease " +
-                    delay + "s, opacity 0.3s ease " + delay + "s";
-            }
-
             // Set _display: block_ if we're showing. If we're hiding
             // we need to wait until the transitions have finished before we
             // set _display: none_.
             if (this.open) {
                 this.style.display = "block";
+            } else {
+                this.async(function() {
+                    this.style.display = "none";
+                }, 500);
             }
-
-            // We have two transitions per child and one for the dialog element
-            // itself. We'll count this down after each _transitionend_ so
-            // we know when we're done animating.
-            this.transCount = items.length * 2 + 1;
 
             // Trigger relayout to make sure all elements have been rendered
             // when applying the transition
@@ -65,25 +45,15 @@
 
             this.fire(this.open ? "open" : "close");
         },
-        /**
-         * Counts down the transition count and, if we are hiding the dialog,
-         * sets _display: none_ once all transitions are through
-         */
-        _transitionEnd: function() {
-            this.transCount--;
-            if (!this.transCount && !this.open) {
-                this.style.display = "none";
-            }
-        },
         _innerTap: function(event) {
             // Intercept the tap event to prevent closing the popup if one
             // of the inner elements was tapped.
             event.stopPropagation();
         },
         //* Closes the popup (duh)
-        close: function() {
+        _close: function() {
             this.open = false;
         }
     });
 
-})(Polymer, padlock.platform);
+})(Polymer);
