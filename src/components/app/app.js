a451c4db168d3221cb0d7ed2f5b4338b9f334da8
Martin Kleinschrodt <martin@maklesoft.com>
2016-06-18 15:59:49 -0300

    Fix typo

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -815,7 +815,7 @@ padlock.App = (function(Polymer, platform, pay) {
                 case padlock.ERR_CLOUD_VERSION_DEPRECATED:
                     this._openForm(
                         [{element: "button", label: "Update Now", tap: this._openAppStore.bind(this), submit: true}],
-                        "A newer version of Padlock is available now! You can download it using the button below." +
+                        "A newer version of Padlock is available now! You can download it using the button below. " +
                         "Please note that you won't be able to use Padlock Cloud until you install the latest version!"
                     );
                     break;
