854247fd4b70d62113f0a1373645150e58016201
Martin Kleinschrodt <martin@maklesoft.com>
2016-11-03 09:53:29 +0100

    Reorganise project structure, add support for electron-builder

---
+++ electron/main.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const {app, shell, BrowserWindow} = require("electron");
+const path = require("path");
+const url = require("url");
+
+// Keep a global reference of the window object, if you don"t, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "app", "index.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+
+    // Open the DevTools.
+    // win.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    win.on("closed", () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+
+    win.webContents.on("new-window", function(e, url) {
+        e.preventDefault();
+        shell.openExternal(url);
+    });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+    app.quit();
+});
