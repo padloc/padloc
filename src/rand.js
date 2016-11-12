883ade01920f6af4da1cb196a9c60362a74f5f18
Steve Thomas <Sc00bz@users.noreply.github.com>
2016-11-11 18:11:13 -0600

    Fixed bias in random

--- src/rand.js
+++ src/rand.js
@@ -21,15 +21,38 @@ padlock.rand = (function() {
         hexa: chars.numbers + "abcdef"
     };
 
+    //* Returns a random uniform value 0 to max, or -1 if max < 0 or max > 0x7fffffff
+    function secureRandom(max) {
+        var rand = new Uint32Array(1);
+        var result;
+
+        max = max | 0;
+        if (max < 0 || max > 0x7fffffff) {
+            return -1;
+        }
+
+        if (((max + 1) & max) === 0) {
+            window.crypto.getRandomValues(rand);
+            result = rand[0] & max;
+        } else {
+            var skip = 0x7fffffff - 0x7fffffff % (max + 1);
+
+            do {
+                window.crypto.getRandomValues(rand);
+                result = rand[0] & 0x7fffffff;
+            } while (result >= skip);
+            result %= max + 1;
+        }
+        return result;
+    }
+
     //* Creates a random string with a given _length_ comprised of given set or characters
     function randomString(length, charSet) {
         length = length || 32;
         charSet = charSet || charSets.full;
-        var rnd = new Uint8Array(length);
         var str = "";
-        window.crypto.getRandomValues(rnd);
         for (var i=0; i<length; i++) {
-            str += charSet[Math.floor(rnd[i] * charSet.length / 256)];
+            str += charSet[secureRandom(charSet.length - 1)];
         }
         return str;
     }
