bee5dbb5a49fc263fc5fd31c70fe86b506c2596f
Martin Kleinschrodt <martin@maklesoft.com>
2017-03-17 18:39:25 -0500

    Calculate record class based on number of rows

--- app/src/ui/list-view/list-view.ts
+++ app/src/ui/list-view/list-view.ts
@@ -15,8 +15,14 @@ export class ListView extends Polymer.Element {
         }
     }
 
+    _nCols: number;
+
     records: Record[];
 
+    connectedCallback() {
+        this._resized();
+        window.addEventListener("resize", this._resized.bind(this));
+    }
     get recordElement(): RecordElement {
         return this.root.querySelector("pl-record") as RecordElement;
     }
@@ -25,11 +31,29 @@ export class ListView extends Polymer.Element {
         return !this.records.length;
     }
 
+    _recordClass(i: number) {
+        const nRow = Math.floor(i / this._nCols);
+
+        if (!(this._nCols % 2) && nRow % 2) {
+            i++;
+        }
+
+        return i % 2 ? "odd" : "even";
     }
 
     _recordTapped({model: {item}}: {model: {item: Record}}) {
         this.recordElement.record = item;
         this.recordElement.open = true;
+    }
+
+    _resized() {
+        const width = this.offsetWidth;
+        const recMinWidth = 250;
+        const nCols = Math.floor(width / recMinWidth);
+        if (nCols !== this._nCols) {
+            this._nCols = nCols;
+            this.notifyPath("records");
+        }
     }
 }
 
