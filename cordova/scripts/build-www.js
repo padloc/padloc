2ba2142a929a115d53b2b8225dbc39b72ca0aa81
Martin Kleinschrodt <martin@maklesoft.com>
2017-05-15 14:20:54 -0600

    Update cordova build process

--- cordova/scripts/build-www.js
+++ cordova/scripts/build-www.js
@@ -2,9 +2,9 @@
 
 "use strict";
 
-var { build } = require("../../lib/build");
+var { buildCordova } = require("../../lib/build");
 var path = require("path");
 
 module.exports = function(context) {
-    build(path.resolve(context.opts.projectRoot, "www"));
+    buildCordova(path.resolve(context.opts.projectRoot, "www"));
 };
