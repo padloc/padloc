2bee4d751bc03c690c7e950b5acfdf95fadaee83
Martin Kleinschrodt <martin@maklesoft.com>
2017-06-23 20:32:01 +0200

    Flesh out / improve update process Add menu items for managing update settings

--- main.js
+++ main.js
@@ -5,41 +5,91 @@ const { autoUpdater } = require("electron-updater");
 const path = require("path");
 const url = require("url");
 const os = require("os");
-const { debug, noupdate, test } = require("yargs").argv;
+const { debug, test } = require("yargs").argv;
+const ElectronStore = require("electron-store");
+
+const settings = new ElectronStore({
+    name: "settings",
+    defaults: {
+        autoDownloadUpdates: false,
+        allowPrerelease: autoUpdater.allowPrerelease
+    }
+});
 
 let win;
+let updateOnQuit = false;
 
-autoUpdater.autoDownload = false;
+function updateReady(updateInfo) {
+    dialog.showMessageBox({
+        message: "Install Update",
+        detail: `Padlock version ${updateInfo.version} has been downloaded. The update will be installed ` +
+            `the next time the app is launched.`,
+        buttons: ["Install Later", "Install And Restart"],
+        defaultId: 1,
+    }, (buttonIndex) => {
+        if (buttonIndex === 1) {
+            autoUpdater.quitAndInstall();
+        } else {
+            updateOnQuit = true;
+        }
+    });
+}
 
-autoUpdater.on("update-available", (updateInfo) => {
+autoUpdater.on("update-downloaded", updateReady);
+
+function htmlToText(html) {
+    return html
+        .replace(/<p>([\w\W]*?)<\/p>/g, "$1")
+        .replace(/<\/?ul>/g, "")
+        .replace(/<li>([\w\W]*?)<\/li>/g, "\u2022 $1");
+}
+
+function updateAvailable(versionInfo) {
+    if (autoUpdater.autoDownload) {
+        return;
+    }
+
     dialog.showMessageBox({
         type: "info",
-        title: `Update Available (Version ${updateInfo.version})`,
-        message: "A new version of Padlock is available! Do you want to install it now?",
-        buttons: ["Download & Install", "Later"]
-    }, (buttonIndex) => {
-        if (buttonIndex === 0) {
+        message: `A new version of Padlock is available! (v${versionInfo.version})`,
+        detail: htmlToText(versionInfo.releaseNotes),
+        checkboxLabel: "Automatically download and install updates in the future (recommended)",
+        buttons: ["Remind Me Later", "Download And Install"],
+        defaultId: 1,
+    }, (buttonIndex, checkboxChecked) => {
+        settings.set("autoDownloadUpdates", checkboxChecked);
+
+        if (buttonIndex === 1) {
             autoUpdater.downloadUpdate();
+
             dialog.showMessageBox({
-                title: "Downloading Update...",
-                type: "info",
-                message: "The new version is being downloaded. You'll be notified when it is ready to be installed!"
+                message: "Downloading Update...",
+                detail: "The new version is being downloaded. You'll be notified when it is ready to be installed!"
             });
         }
     });
-});
+}
 
-autoUpdater.on("update-downloaded", (updateInfo) => {
-    dialog.showMessageBox({
-        title: "Install Update",
-        message: `Padlock version ${updateInfo.version} has been downloaded and is ready to be installed!`,
-        buttons: ["Quit & Install", "Cancel"]
-    }, (buttonIndex) => {
-        if (buttonIndex === 0) {
-            autoUpdater.quitAndInstall();
+function checkForUpdates(manual) {
+    autoUpdater.autoDownload = settings.get("autoDownloadUpdates");
+    autoUpdater.allowPrerelease = settings.get("allowPrerelease");
+
+    autoUpdater.checkForUpdates().then((result) => {
+        if (result.fileInfo) {
+            updateAvailable(result.versionInfo);
+        } else if (manual) {
+            dialog.showMessageBox({
+                type: "info",
+                message: "No Updates Available",
+                detail: "Your version of Padlock is up to date.",
+                checkboxLabel: "Automatically download and install updates in the future (recommended)",
+                checkboxChecked: settings.get("autoDownloadUpdates")
+            }, (buttonIndex, checkboxChecked) => {
+                settings.set("autoDownloadUpdates", checkboxChecked);
+            });
         }
     });
-});
+}
 
 function createWindow() {
     // Create the browser window.
@@ -72,24 +122,59 @@ function createWindow() {
         shell.openExternal(url);
     });
 
-    win.webContents.on("did-frame-finish-load", function() {
-        if (!noupdate) {
-            autoUpdater.checkForUpdates();
-        }
-    });
-
     win.once("ready-to-show", () => win.show());
 }
 
 function createApplicationMenu() {
+    const checkForUpdatesItem = {
+        label: "Check for Updates...",
+        click() { checkForUpdates(true); }
+    };
+
+    const appSubMenu = os.platform() === "darwin" ? [ { role: "about" } ] :
+        [ { label: `Padlock v${app.getVersion()}`, enabled: false} ];
+
+    appSubMenu.push(
+        checkForUpdatesItem,
+        { type: "separator" },
+        { role: "quit" }
+    );
+
     // Set up menu
     const template = [
         {
             label: "Application",
+            submenu: appSubMenu
+        },
+        {
+            label: "Settings",
             submenu: [
-                { role: "about" },
-                { type: "separator" },
-                { role: "quit" }
+                {
+                    label: "Updates",
+                    submenu: [
+                        checkForUpdatesItem,
+                        { type: "separator" },
+                        {
+                            type: "checkbox",
+                            label: "Automatically Download and Install Updates",
+                            checked: settings.get("autoDownloadUpdates"),
+                            click(item) { settings.set("autoDownloadUpdates", item.checked); }
+                        },
+                        { type: "separator" },
+                        {
+                            type: "radio",
+                            label: "Only Download Stable Releases (recommended)",
+                            checked: !settings.get("allowPrerelease"),
+                            click(item) { settings.set("allowPrerelease", !item.checked); }
+                        },
+                        {
+                            type: "radio",
+                            label: "Download Stable and Beta Releases",
+                            checked: settings.get("allowPrerelease"),
+                            click(item) { settings.set("allowPrerelease", item.checked); }
+                        }
+                    ]
+                }
             ]
         },
         {
@@ -114,10 +199,9 @@ function createApplicationMenu() {
 // Some APIs can only be used after this event occurs.
 app.on("ready", () => {
     createWindow();
-    // Create application on macOS
-    if (os.platform() === "darwin") {
-        createApplicationMenu();
-    }
+    createApplicationMenu();
+
+    checkForUpdates();
 });
 
 // Quit when all windows are closed.
@@ -128,5 +212,13 @@ app.on("window-all-closed", () => {
 app.on("activate", () => {
     if (win === null) {
         createWindow();
+    }
+});
+
+app.on("before-quit", (e) => {
+    if (updateOnQuit) {
+        updateOnQuit = false;
+        e.preventDefault();
+        autoUpdater.quitAndInstall();
     }
 });
