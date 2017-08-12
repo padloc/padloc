02fe5058fd158c88968f5bcc261fe86fd944e404
Martin Kleinschrodt <martin@maklesoft.com>
2017-08-12 10:08:01 +0200

    Don't bundle files for Android build

--- cordova/scripts/build-www.js
+++ cordova/scripts/build-www.js
@@ -6,5 +6,10 @@ var { buildCordova } = require("../../lib/build");
 var path = require("path");
 
 module.exports = function(context) {
-    return buildCordova(path.resolve(context.opts.projectRoot, "www"));
+    const platforms = context.opts.cordova.platforms;
+    if (platforms.length > 1) {
+        console.error("Can't build for more than one platform at once!");
+        process.exit(1);
+    }
+    return buildCordova(platforms[0], path.resolve(context.opts.projectRoot, "www"));
 };
