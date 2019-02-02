481f3096734e4e8c3d27c6162c9e6a78a4627196
Martin Kleinschrodt <martin@maklesoft.com>
2019-02-02 18:43:49 +0100

    Remove S3AttachmentStorage for now

--- packages/server/src/attachment.ts
+++ packages/server/src/attachment.ts
@@ -1,62 +1,62 @@
-import { S3 } from "aws-sdk";
+// import { S3 } from "aws-sdk";
 import { join } from "path";
 import { readFile, writeFile, ensureDir, remove, readdir, stat } from "fs-extra";
 import { marshal, unmarshal, bytesToString, stringToBytes } from "@padloc/core/src/encoding";
 import { Attachment, AttachmentStorage } from "@padloc/core/src/attachment";
 import { Vault } from "@padloc/core/src/vault";
-import { Err, ErrorCode } from "@padlock/core/src/error";
+import { Err, ErrorCode } from "@padloc/core/src/error";
 
-export interface S3Config {
-    bucket: string;
-}
-
-export class S3Storage implements AttachmentStorage {
-    private _s3: S3;
-
-    private _getKey(att: Attachment) {
-        return `${att.vault}_${att.id}`;
-    }
-
-    constructor(public config: S3Config) {
-        this._s3 = new S3();
-    }
-
-    async get(att: Attachment) {
-        const obj = await this._s3
-            .getObject({
-                Bucket: this.config.bucket,
-                Key: this._getKey(att)
-            })
-            .promise();
-
-        const body = obj.Body as Uint8Array;
-        await att.deserialize(unmarshal(bytesToString(body)));
-        return att;
-    }
-
-    async put(att: Attachment) {
-        await this._s3.upload({
-            Bucket: this.config.bucket,
-            Key: this._getKey(att),
-            Body: stringToBytes(marshal(await att.serialize()))
-        });
-    }
-
-    async delete(att: Attachment) {
-        await this._s3.deleteObject({
-            Bucket: this.config.bucket,
-            Key: this._getKey(att)
-        });
-    }
-
-    async deleteAll() {
-        throw new Err(ErrorCode.NOT_SUPPORTED);
-    }
-
-    async getUsage(): Promise<number> {
-        throw new Err(ErrorCode.NOT_SUPPORTED);
-    }
-}
+// export interface S3Config {
+//     bucket: string;
+// }
+//
+// export class S3Storage implements AttachmentStorage {
+//     private _s3: S3;
+//
+//     private _getKey(att: Attachment) {
+//         return `${att.vault}_${att.id}`;
+//     }
+//
+//     constructor(public config: S3Config) {
+//         this._s3 = new S3();
+//     }
+//
+//     async get(att: Attachment) {
+//         const obj = await this._s3
+//             .getObject({
+//                 Bucket: this.config.bucket,
+//                 Key: this._getKey(att)
+//             })
+//             .promise();
+//
+//         const body = obj.Body as Uint8Array;
+//         await att.deserialize(unmarshal(bytesToString(body)));
+//         return att;
+//     }
+//
+//     async put(att: Attachment) {
+//         await this._s3.upload({
+//             Bucket: this.config.bucket,
+//             Key: this._getKey(att),
+//             Body: stringToBytes(marshal(await att.serialize()))
+//         });
+//     }
+//
+//     async delete(att: Attachment) {
+//         await this._s3.deleteObject({
+//             Bucket: this.config.bucket,
+//             Key: this._getKey(att)
+//         });
+//     }
+//
+//     async deleteAll() {
+//         throw new Err(ErrorCode.NOT_SUPPORTED);
+//     }
+//
+//     async getUsage(): Promise<number> {
+//         throw new Err(ErrorCode.NOT_SUPPORTED);
+//     }
+// }
 
 export interface FSStorageConfig {
     path: string;
