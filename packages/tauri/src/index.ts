43855edff71d6d6de5c8e411d40d8fa905a15d40
Martin Kleinschrodt <martin@maklesoft.com>
2020-07-30 17:37:55 +0200

    Fix problem with window load outracing tauri init script

--- packages/tauri/src/index.ts
+++ packages/tauri/src/index.ts
@@ -1,13 +1,23 @@
 import { setPlatform } from "@padloc/core/src/platform";
 import { TauriPlatform } from "./platform";
 
+window.onload = () => console.log("window loaded");
+
+function createApp() {
+    console.log("creating app component");
+    const app = document.createElement("pl-app");
+    document.body.appendChild(app);
+}
+
 (async () => {
     setPlatform(new TauriPlatform());
 
     await import("@padloc/app/src/elements/app");
 
-    window.onload = () => {
-        const app = document.createElement("pl-app");
-        document.body.appendChild(app);
-    };
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", createApp);
+    } else {
+        createApp();
+    }
 })();
