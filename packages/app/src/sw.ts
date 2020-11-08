a0ce5c3da2b260c67d6c3028df5d38780dac66aa
dome4 <dominic.seitz@gmx.de>
2020-11-08 15:57:42 +0100

    Fix sw.ts

--- packages/app/src/sw.ts
+++ packages/app/src/sw.ts
@@ -1,7 +1,5 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-declare const workbox: typeof import("workbox-sw");
-
 // @ts-ignore
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
