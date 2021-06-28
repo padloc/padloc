238b53ad97796e70dc50273c03d59ccd8d79d3a0
Martin Kleinschrodt <martin@maklesoft.com>
2021-06-28 18:08:34 +0200

    Add tauri:dev command

--- packages/tauri/webpack.config.js
+++ packages/tauri/webpack.config.js
@@ -13,50 +13,50 @@ module.exports = {
         path: out,
         filename: "[name].js",
         chunkFilename: "[name].chunk.js",
-        publicPath: "/"
+        publicPath: "/",
     },
     mode: "development",
     devtool: "source-map",
     stats: "minimal",
     resolve: {
-        extensions: [".ts", ".js"]
+        extensions: [".ts", ".js"],
     },
     module: {
         rules: [
             {
                 test: /\.ts$/,
-                loader: "ts-loader"
+                loader: "ts-loader",
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-                use: ["file-loader"]
-            }
-        ]
+                use: ["file-loader"],
+            },
+        ],
     },
     plugins: [
         new EnvironmentPlugin({
             PL_SERVER_URL: serverUrl,
             PL_BILLING_ENABLED: null,
-            PL_BILLING_DISABLE_PAYMENT: null,
+            PL_BILLING_DISABLE_PAYMENT: true,
             PL_BILLING_STRIPE_PUBLIC_KEY: null,
             PL_SUPPORT_EMAIL: "support@padloc.app",
             PL_VERSION: version,
-            PL_DISABLE_SW: true
+            PL_DISABLE_SW: true,
         }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: "Padloc",
-            template: path.resolve(__dirname, "src/index.html")
-        })
+            template: path.resolve(__dirname, "src/index.html"),
+        }),
     ],
     devServer: {
         contentBase: path.resolve(__dirname, "dist"),
         historyApiFallback: true,
         host: "0.0.0.0",
-        port: process.env.PL_PWA_PORT || 8080
-    }
+        port: process.env.PL_PWA_PORT || 8080,
+    },
 };
