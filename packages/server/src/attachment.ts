26a6aa32bf49af3ffd2bbccb770f6e1072439a84
Martin Kleinschrodt <martin@maklesoft.com>
2021-08-12 10:27:00 +0200

    Add experimental S3 attachment storage engine

--- packages/server/src/attachment.ts
+++ packages/server/src/attachment.ts
@@ -4,58 +4,107 @@ import { readFile, writeFile, ensureDir, remove, readdir, stat } from "fs-extra"
 import { Attachment, AttachmentID, AttachmentStorage } from "@padloc/core/src/attachment";
 import { VaultID } from "@padloc/core/src/vault";
 import { Err, ErrorCode } from "@padloc/core/src/error";
+import {
+    DeleteObjectCommand,
+    DeleteObjectsCommand,
+    GetObjectCommand,
+    ListObjectsCommand,
+    ObjectIdentifier,
+    PutObjectCommand,
+    S3Client,
+} from "@aws-sdk/client-s3";
+import { Readable } from "stream";
 
-// export interface S3Config {
-//     bucket: string;
-// }
-//
-// export class S3Storage implements AttachmentStorage {
-//     private _s3: S3;
-//
-//     private _getKey(att: Attachment) {
-//         return `${att.vault}_${att.id}`;
-//     }
-//
-//     constructor(public config: S3Config) {
-//         this._s3 = new S3();
-//     }
-//
-//     async get(att: Attachment) {
-//         const obj = await this._s3
-//             .getObject({
-//                 Bucket: this.config.bucket,
-//                 Key: this._getKey(att)
-//             })
-//             .promise();
-//
-//         const body = obj.Body as Uint8Array;
-//         await att.deserialize(unmarshal(bytesToString(body)));
-//         return att;
-//     }
-//
-//     async put(att: Attachment) {
-//         await this._s3.upload({
-//             Bucket: this.config.bucket,
-//             Key: this._getKey(att),
-//             Body: stringToBytes(marshal(await att.serialize()))
-//         });
-//     }
-//
-//     async delete(att: Attachment) {
-//         await this._s3.deleteObject({
-//             Bucket: this.config.bucket,
-//             Key: this._getKey(att)
-//         });
-//     }
-//
-//     async deleteAll() {
-//         throw new Err(ErrorCode.NOT_SUPPORTED);
-//     }
-//
-//     async getUsage(): Promise<number> {
-//         throw new Err(ErrorCode.NOT_SUPPORTED);
-//     }
-// }
+function streamToBytes(stream: Readable): Promise<Uint8Array> {
+    return new Promise<Uint8Array>((resolve, reject) => {
+        const chunks: any[] = [];
+        stream.on("data", (chunk) => chunks.push(chunk));
+        stream.on("error", reject);
+        stream.on("end", () => resolve(new Uint8Array(Buffer.concat(chunks))));
+    });
+}
+
+export interface S3Config {
+    region: string;
+    bucket: string;
+    endpoint: string;
+    accessKeyId: string;
+    secretAccessKey: string;
+}
+
+export class S3Storage implements AttachmentStorage {
+    private _client: S3Client;
+
+    constructor(public config: S3Config) {
+        this._client = new S3Client({
+            region: this.config.region,
+            endpoint: this.config.endpoint,
+            credentials: {
+                accessKeyId: this.config.accessKeyId,
+                secretAccessKey: this.config.secretAccessKey,
+            },
+        });
+    }
+
+    async get(vault: VaultID, id: AttachmentID) {
+        const obj = await this._client.send(
+            new GetObjectCommand({
+                Bucket: this.config.bucket,
+                Key: `${vault}/${id}`,
+            })
+        );
+
+        const bytes = await streamToBytes(obj.Body as Readable);
+        return new Attachment().fromBytes(bytes);
+    }
+
+    async put(att: Attachment) {
+        await this._client.send(
+            new PutObjectCommand({
+                Bucket: this.config.bucket,
+                Key: `${att.vault}/${att.id}`,
+                Body: att.toBytes(),
+            })
+        );
+    }
+
+    async delete(vault: VaultID, id: AttachmentID) {
+        await this._client.send(
+            new DeleteObjectCommand({
+                Bucket: this.config.bucket,
+                Key: `${vault}/${id}`,
+            })
+        );
+    }
+
+    async deleteAll(vault: VaultID) {
+        const list = await this._client.send(
+            new ListObjectsCommand({
+                Bucket: this.config.bucket,
+                Prefix: vault,
+            })
+        );
+        if (!list.Contents) {
+            return;
+        }
+        await this._client.send(
+            new DeleteObjectsCommand({
+                Bucket: this.config.bucket,
+                Delete: { Objects: list.Contents as ObjectIdentifier[] },
+            })
+        );
+    }
+
+    async getUsage(vault: VaultID): Promise<number> {
+        const list = await this._client.send(
+            new ListObjectsCommand({
+                Bucket: this.config.bucket,
+                Prefix: vault,
+            })
+        );
+        return list.Contents?.reduce((total, entry) => total + (entry.Size || 0), 0) || 0;
+    }
+}
 
 export interface FSStorageConfig {
     path: string;
