6ee9b7ecea340f73d3ee8cf39d0f9c7f4eec67e8
Martin Kleinschrodt <martin@maklesoft.com>
2021-12-25 10:33:24 +0100

    Add job for building signed and notarized dmg for macos

--- packages/electron/scripts/notarize.js
+++ packages/electron/scripts/notarize.js
@@ -5,8 +5,8 @@ exports.default = async function notarizing(context) {
     const rootDir = resolve(__dirname, "../../..");
     const assetsDir = resolve(rootDir, process.env.PL_ASSETS_DIR || "assets");
     const { appId } = require(join(assetsDir, "manifest.json"));
-    const appleId = process.env.PL_NOTARIZE_APPLE_ID;
-    const appleIdPassword = process.env.PL_NOTARIZE_APPLE_ID_PASSWORD;
+    const appleId = process.env.PL_MACOS_NOTARIZE_APPLE_ID;
+    const appleIdPassword = process.env.PL_MACOS_NOTARIZE_PASSWORD;
 
     if (!appleId) {
         console.warn("Skipping Notarization - No Apple ID provided");
