73304830b14d5160f772bf7f45c4adb826b77b3b
Bruno Bernardino <me@brunobernardino.com>
2021-12-27 12:26:54 +0000

    Tauri now builds and works!
    Also moved the icons back, because the build doesn't respect the paths sent

--- packages/tauri/webpack.config.js
+++ packages/tauri/webpack.config.js
@@ -8,7 +8,7 @@ const out = process.env.PL_PWA_DIR || resolve(__dirname, "dist");
 const serverUrl = process.env.PL_SERVER_URL || `http://0.0.0.0:${process.env.PL_SERVER_PORT || 3000}`;
 const rootDir = resolve(__dirname, "../..");
 const assetsDir = resolve(rootDir, process.env.PL_ASSETS_DIR || "assets");
-const { name } = require(join(assetsDir, "manifest.json"));
+const { name, version: vendorVersion } = require(join(assetsDir, "manifest.json"));
 
 module.exports = {
     entry: resolve(__dirname, "src/index.ts"),
@@ -49,13 +49,16 @@ module.exports = {
     },
     plugins: [
         new EnvironmentPlugin({
+            PL_APP_NAME: name,
             PL_SERVER_URL: serverUrl,
             PL_BILLING_ENABLED: null,
-            PL_BILLING_DISABLE_PAYMENT: true,
+            PL_BILLING_DISABLE_PAYMENT: null,
             PL_BILLING_STRIPE_PUBLIC_KEY: null,
             PL_SUPPORT_EMAIL: "support@padloc.app",
             PL_VERSION: version,
-            PL_DISABLE_SW: true,
+            PL_VENDOR_VERSION: vendorVersion || version,
+            PL_DISABLE_SW: false,
+            PL_CLIENT_SUPPORTED_AUTH_TYPES: "email",
         }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
@@ -64,11 +67,11 @@ module.exports = {
         }),
     ],
     devServer: {
-        static: {
-            directory: resolve(__dirname, "dist"),
-        },
         historyApiFallback: true,
         host: "0.0.0.0",
         port: process.env.PL_PWA_PORT || 8080,
+        // hot: false,
+        // liveReload: false,
+        client: { overlay: false },
     },
 };
