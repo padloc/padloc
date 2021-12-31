8e80b688fe41d2db09ef0b4fdc7dea96ac182843
Bruno Bernardino <me@brunobernardino.com>
2021-12-31 20:42:52 +0000

    Tweak build and release making.

--- packages/tauri/generate-tauri-update-file.js
+++ packages/tauri/generate-tauri-update-file.js
@@ -5,20 +5,20 @@ const tauriUpdateFilePath = resolve(__dirname, "tauri-update.json");
 const packageFilePath = resolve(__dirname, "package.json");
 
 const tauriUpdate = {
-    name: "v[VERSION]",
+    name: "[VERSION]",
     pub_date: "[DATE]",
     platforms: {
         darwin: {
-            signature: "https://github.com/padloc/padloc/releases/download/v[VERSION]/Padloc.app.tar.gz.sig",
-            url: "https://github.com/padloc/padloc/releases/download/v[VERSION]/Padloc.app.tar.gz",
+            signature: "[URL]",
+            url: "[URL]",
         },
         linux: {
-            signature: "https://github.com/padloc/padloc/releases/download/v[VERSION]/Padloc.AppImage.tar.gz.sig",
-            url: "https://github.com/padloc/padloc/releases/download/v[VERSION]/Padloc.AppImage.tar.gz",
+            signature: "[URL]",
+            url: "[URL]",
         },
         win64: {
-            signature: "https://github.com/padloc/padloc/releases/download/v[VERSION]/Padloc.x64.msi.zip.sig",
-            url: "https://github.com/padloc/padloc/releases/download/v[VERSION]/Padloc.x64.msi.zip",
+            signature: "[URL]",
+            url: "[URL]",
         },
     },
 };
@@ -33,9 +33,9 @@ tauriUpdate.name = `v${version}`;
 tauriUpdate.pub_date = now;
 tauriUpdate.platforms.darwin.url = `https://github.com/padloc/padloc/releases/download/v${version}/Padloc.app.tar.gz`;
 tauriUpdate.platforms.darwin.signature = `${tauriUpdate.platforms.darwin.url}.sig`;
-tauriUpdate.platforms.linux.url = `https://github.com/padloc/padloc/releases/download/v${version}/Padloc.AppImage.tar.gz`;
+tauriUpdate.platforms.linux.url = `https://github.com/padloc/padloc/releases/download/v${version}/padloc_${version}_amd64.AppImage.tar.gz`;
 tauriUpdate.platforms.linux.signature = `${tauriUpdate.platforms.linux.url}.sig`;
-tauriUpdate.platforms.win64.url = `https://github.com/padloc/padloc/releases/download/v${version}/Padloc.x64.msi.zip`;
+tauriUpdate.platforms.win64.url = `https://github.com/padloc/padloc/releases/download/v${version}/Padloc_${version}_x64.msi.zip`;
 tauriUpdate.platforms.win64.signature = `${tauriUpdate.platforms.win64.url}.sig`;
 
 writeFileSync(tauriUpdateFilePath, JSON.stringify(tauriUpdate, null, 4), "utf-8");
