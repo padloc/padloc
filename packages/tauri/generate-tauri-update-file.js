e3c634374c5c2a70b7d628cb282beeee87ae6f18
Bruno Bernardino <me@brunobernardino.com>
2022-01-03 21:46:16 +0000

    Implement review recommendations

--- packages/tauri/generate-tauri-update-file.js
+++ packages/tauri/generate-tauri-update-file.js
@@ -1,38 +1,30 @@
 const { resolve } = require("path");
-const { readFileSync, writeFileSync } = require("fs");
+const { writeFileSync } = require("fs");
 
 const tauriUpdateFilePath = resolve(__dirname, "tauri-update.json");
 const packageFilePath = resolve(__dirname, "package.json");
 
-const tauriUpdate = {
-    name: "[VERSION]",
-    pub_date: "[DATE]",
-    platforms: {
-        darwin: {
-            signature: "[URL]",
-            url: "[URL]",
-        },
-        linux: {
-            signature: "[URL]",
-            url: "[URL]",
-        },
-        win64: {
-            signature: "[URL]",
-            url: "[URL]",
-        },
-    },
-};
-
 const { version } = require(packageFilePath);
 const now = new Date().toISOString();
 
-tauriUpdate.name = `v${version}`;
-tauriUpdate.pub_date = now;
-tauriUpdate.platforms.darwin.url = `https://github.com/padloc/padloc/releases/download/v${version}/Padloc.app.tar.gz`;
-tauriUpdate.platforms.darwin.signature = `${tauriUpdate.platforms.darwin.url}.sig`;
-tauriUpdate.platforms.linux.url = `https://github.com/padloc/padloc/releases/download/v${version}/padloc_${version}_amd64.AppImage.tar.gz`;
-tauriUpdate.platforms.linux.signature = `${tauriUpdate.platforms.linux.url}.sig`;
-tauriUpdate.platforms.win64.url = `https://github.com/padloc/padloc/releases/download/v${version}/Padloc_${version}_x64.msi.zip`;
-tauriUpdate.platforms.win64.signature = `${tauriUpdate.platforms.win64.url}.sig`;
+const baseUrl = `https://github.com/padloc/padloc/releases/download/v${version}`;
+const tauriUpdate = {
+    name: `v${version}`,
+    pub_date: now,
+    platforms: {
+        darwin: {
+            url: `${baseUrl}/Padloc.app.tar.gz`,
+            signature: `${baseUrl}/Padloc.app.tar.gz.sig`,
+        },
+        linux: {
+            url: `${baseUrl}/padloc_${version}_amd64.AppImage.tar.gz`,
+            signature: `${baseUrl}/padloc_${version}_amd64.AppImage.tar.gz.sig`,
+        },
+        win64: {
+            url: `${baseUrl}/Padloc_${version}_x64.msi.zip`,
+            signature: `${baseUrl}/Padloc_${version}_x64.msi.zip.sig`,
+        },
+    },
+};
 
 writeFileSync(tauriUpdateFilePath, JSON.stringify(tauriUpdate, null, 4), "utf-8");
