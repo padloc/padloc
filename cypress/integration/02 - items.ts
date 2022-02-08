e2cd6135d5b81e671be1f83e336249be96f7dc30
Bruno Bernardino <me@brunobernardino.com>
2022-02-08 16:20:01 +0000

    Increase the time before new item form shows up

--- cypress/integration/02 - items.ts
+++ cypress/integration/02 - items.ts
@@ -24,7 +24,7 @@ describe("Items", () => {
         cy.url().should("include", "/new");
 
         // Give the app some time to finish animations
-        cy.wait(200);
+        cy.wait(300);
 
         // Fill in form
         cy.get("pl-app")
