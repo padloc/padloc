f04dc2b1d0ee93339bcdcd5a75724eba8fef96e8
Martin Kleinschrodt <martin@maklesoft.com>
2022-05-06 18:09:50 +0200

    Add missing properties in AccountProvisioning and OrgProvisioning classes (forgot to commit earlier)

--- packages/core/src/provisioning.ts
+++ packages/core/src/provisioning.ts
@@ -82,6 +82,9 @@ export class AccountFeatures extends Serializable {
     attachments: Feature = new Feature();
 
     @AsSerializable(Feature)
+    securityCheck: Feature = new Feature();
+
+    @AsSerializable(Feature)
     billing: Feature = new Feature();
 }
 
@@ -102,6 +105,9 @@ export class OrgFeatures extends Serializable {
 
     @AsSerializable(OrgFeature)
     attachments: OrgFeature = new OrgFeature();
+
+    @AsSerializable(Feature)
+    securityCheck: Feature = new Feature();
 }
 
 export class AccountProvisioning extends Storable {
