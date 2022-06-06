08f0492d67e390c978ed5df71fb294a7ccc663a1
Bruno Bernardino <me@brunobernardino.com>
2022-06-06 12:00:55 +0100

    Support exporting all vaults
    Fixes #253
    
    This basically creates a `.zip` file with all the vault export files inside.
    
    I also updated the dependency version because on 3.8.0 there was an important security fix.

--- packages/app/src/elements/export-dialog.ts
+++ packages/app/src/elements/export-dialog.ts
@@ -2,6 +2,7 @@ import { Vault } from "@padloc/core/src/vault";
 import { translate as $l } from "@padloc/locale/src/translate";
 import { saveFile } from "@padloc/core/src/platform";
 import { stringToBytes } from "@padloc/core/src/encoding";
+import JSZip from "jszip";
 import { CSV, PBES2 } from "../lib/import";
 import { supportedFormats, asCSV, asPBES2Container } from "../lib/export";
 import { app } from "../globals";
@@ -11,21 +12,34 @@ import { Dialog } from "./dialog";
 import { html } from "lit";
 import { customElement, query } from "lit/decorators.js";
 
+const ALL_VAULTS_OPTION = $l("All Vaults");
+
 @customElement("pl-export-dialog")
 export class ExportDialog extends Dialog<void, void> {
     @query("#formatSelect")
     private _formatSelect: Select<string>;
     @query("#vaultSelect")
-    private _vaultSelect: Select<Vault>;
+    private _vaultSelect: Select<Vault | string>;
 
     renderContent() {
+        let itemCount: string = "0";
+
+        if (this._vaultSelect?.value !== ALL_VAULTS_OPTION) {
+            const vault = this._vaultSelect?.value as Vault;
+            if (vault) {
+                itemCount = vault.items.size.toString();
+            }
+        } else if (this._vaultSelect?.value === ALL_VAULTS_OPTION) {
+            itemCount = $l("All");
+        }
+
         return html`
             <div class="padded vertical spacing layout">
                 <h1 class="big margined text-centering">${$l("Export Data")}</h1>
 
                 <pl-select
                     id="vaultSelect"
-                    .options=${app.vaults.map((v) => ({ value: v }))}
+                    .options=${[ALL_VAULTS_OPTION, ...app.vaults].map((v) => ({ value: v }))}
                     .label=${$l("Target Vault")}
                     @change=${() => this.requestUpdate()}
                 >
@@ -40,15 +54,13 @@ export class ExportDialog extends Dialog<void, void> {
 
                 <div class="small padded" ?hidden=${this._formatSelect && this._formatSelect.value !== CSV.value}>
                     ${$l(
-                        "WARNING: Exporting to CSV format will save your data without encyryption of any " +
-                            "kind which means it can be read by anyone. We strongly recommend exporting your data as " +
-                            "a secure, encrypted file, instead!"
+                        `WARNING: Exporting to CSV format will save your data without encryption of any kind which means it can be read by anyone. We strongly recommend exporting your data as "a secure, encrypted file, instead!`
                     )}
                 </div>
 
                 <div class="horizontal evenly stretching spacing layout">
                     <pl-button class="primary" @click=${() => this._export()}>
-                        ${$l("Export {0} Items", this._vaultSelect?.value?.items.size.toString() || "0")}
+                        ${$l("Export {0} Items", itemCount)}
                     </pl-button>
                     <pl-button @click=${this.dismiss}> ${$l("Cancel")} </pl-button>
                 </div>
@@ -63,11 +75,9 @@ export class ExportDialog extends Dialog<void, void> {
         return super.show();
     }
 
-    private async _export() {
-        const vault = this._vaultSelect.value!;
+    private async _exportData(vault: Vault, password?: string) {
         const items = [...vault.items];
-
-        const date = new Date().toISOString().substr(0, 10);
+        const date = new Date().toISOString().substring(0, 10);
         let data = "";
         let fileName = "";
         let type = "text/plain";
@@ -81,7 +91,72 @@ export class ExportDialog extends Dialog<void, void> {
 
             case PBES2.value:
                 this.open = false;
-                const password = await prompt($l("Please choose a password to protect this backup with!"), {
+                if (!password) {
+                    password = await prompt($l("Please choose a password to protect this backup with!"), {
+                        title: $l("Choose Password"),
+                        type: "password",
+                        placeholder: "Enter Password",
+                        validate: async (val) => {
+                            if (!val) {
+                                throw $l("Please choose a password!");
+                            }
+                            return val;
+                        },
+                    });
+
+                    if (!password) {
+                        this.open = true;
+                        return;
+                    }
+
+                    const repeated = await prompt($l("Please repeat the password!"), {
+                        title: $l("Choose Password"),
+                        type: "password",
+                        placeholder: "Repeat Password",
+                        validate: async (val) => {
+                            if (val !== password) {
+                                throw $l("Password not repeated correctly!");
+                            }
+                            return val;
+                        },
+                    });
+
+                    if (!repeated) {
+                        this.open = true;
+                        return;
+                    }
+                }
+
+                data = await asPBES2Container(items, password);
+                fileName = `${vault.name.replace(/ /g, "_")}_${date}.pbes2`;
+                break;
+
+            default:
+                return;
+        }
+
+        return {
+            data,
+            fileName,
+            type,
+        };
+    }
+
+    private async _export() {
+        const vaultOption = this._vaultSelect.value!;
+
+        if (vaultOption === ALL_VAULTS_OPTION) {
+            const zip = new JSZip();
+
+            const date = new Date().toISOString().substring(0, 10);
+            let zipFileName = `All_Vaults_${date}.zip`;
+            let zipFileType = "application/zip";
+
+            // Ask for password once
+            let password = "";
+            if (this._formatSelect.value === PBES2.value) {
+                this.open = false;
+                password = await prompt($l("Please choose a password to protect this backup with!"), {
                     title: $l("Choose Password"),
                     type: "password",
                     placeholder: "Enter Password",
@@ -114,15 +189,27 @@ export class ExportDialog extends Dialog<void, void> {
                     this.open = true;
                     return;
                 }
+            }
 
-                data = await asPBES2Container(items, password);
-                fileName = `${vault.name.replace(/ /g, "_")}_${date}.pbes2`;
-                break;
+            for (const vault of app.vaults) {
+                const result = await this._exportData(vault, password);
+                if (result) {
+                    const { data, fileName } = result;
+                    zip.file(fileName, stringToBytes(data));
+                }
+            }
 
-            default:
-                return;
+            const zipData = await zip.generateAsync({ type: "uint8array" });
+
+            saveFile(zipFileName, zipFileType, zipData);
+        } else {
+            const result = await this._exportData(this._vaultSelect.value! as Vault);
+            if (result) {
+                const { data, fileName, type } = result;
+                saveFile(fileName, type, stringToBytes(data));
+            }
         }
-        saveFile(fileName, type, stringToBytes(data));
+
         this.done();
     }
 }
