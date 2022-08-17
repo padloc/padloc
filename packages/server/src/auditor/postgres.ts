a7e61ef22dee14eb5b67e258fba2c130eb8358ba
Bruno Bernardino <me@brunobernardino.com>
2022-08-17 12:35:07 +0100

    Fix type problem for postgres

--- packages/server/src/auditor/postgres.ts
+++ packages/server/src/auditor/postgres.ts
@@ -6,7 +6,7 @@ export class PostgresAuditor implements Auditor {
 
     log(kind: AuditKind, id: string, modifierAccountId: string, type: AuditChangeType, oldData: any, newData: any) {
         const auditLog = new LogChange(kind, id, modifierAccountId, type, oldData, newData);
-        auditLog.id = `${auditLog.time.toISOString()}_${Math.floor(Math.random() * 1e6)}`;
+        auditLog.id = `${auditLog.date.toISOString()}_${Math.floor(Math.random() * 1e6)}`;
         (async () => {
             try {
                 this._storage.save(auditLog);
