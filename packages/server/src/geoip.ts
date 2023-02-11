255b9412a97f3dd036ef042042a08425e89cd9f3
Bruno Bernardino <me@brunobernardino.com>
2023-02-11 12:20:36 +0000

    Don't fail when locations aren't fetched

--- packages/server/src/geoip.ts
+++ packages/server/src/geoip.ts
@@ -4,10 +4,17 @@ import maxmind, { CityResponse } from "maxmind";
 let lookupPromise = getLookup();
 
 export async function getLookup() {
-    await geolite2.downloadDbs();
-    return geolite2.open<CityResponse>("GeoLite2-City", (path) => {
-        return maxmind.open(path);
-    });
+    try {
+        await geolite2.downloadDbs();
+        return geolite2.open<CityResponse>("GeoLite2-City", (path) => {
+            return maxmind.open(path);
+        });
+    } catch (error) {
+        console.log(error);
+        return {
+            get: () => null,
+        };
+    }
 }
 
 export async function getLocation(ip: string) {
