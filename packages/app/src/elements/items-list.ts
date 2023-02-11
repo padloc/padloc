c30f9b12a9326ac621f70e2a40d46d956f0e85a6
Bruno Bernardino <me@brunobernardino.com>
2023-02-11 10:05:14 +0000

    Improved search
    This makes it possible to search for things like "Google - Padloc" with a term like "google padloc" instead of "google - padloc".

--- packages/app/src/elements/items-list.ts
+++ packages/app/src/elements/items-list.ts
@@ -52,7 +52,19 @@ function filterByString(fs: string, rec: VaultItem) {
     const content = [rec.name, ...rec.tags, ...rec.fields.map((f) => f.name), ...rec.fields.map((f) => f.value)]
         .join(" ")
         .toLowerCase();
-    return content.search(escapeRegex(fs.toLowerCase())) !== -1;
+    const searchTerms = fs.toLowerCase().split(" ").filter(Boolean);
+
+    if (searchTerms.length <= 1) {
+        return content.search(escapeRegex(fs.toLowerCase())) !== -1;
+    }
+
+    for (const searchTerm of searchTerms) {
+        if (!content.includes(searchTerm)) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 @customElement("pl-vault-item-list-item")
